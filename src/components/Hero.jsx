531816c4fad664ae9d9f04dd1c1291acc443c4fd
Koen De Groot <koendegroot84@gmail.com>
2025-11-01 16:49:50 +0100

    Enhance Hero component with GSAP animations and content updates
    - Integrated GSAP for animations in the Hero component, including character splitting and staggered effects.
    - Updated the hero image source to a static image.
    - Added a new subtitle and a button to the Hero section for improved user engagement.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,49 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+
 const Hero = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create(".hero-title", {
+      type: "chars",
+    });
+
+    const tl = gsap.timeline({ delay: 1 });
+
+    tl.to(".hero-content", {
+      opacity: 1,
+      y: 0,
+      ease: "power1.inOut",
+    })
+      .to(
+        ".hero-text-scroll",
+        {
+          opacity: 1,
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          ease: "circ.out",
+        },
+        "-=0.5"
+      )
+      .from(
+        titleSplit.chars,
+        {
+          yPercent: 200,
+          stagger: 0.02,
+          ease: "power2.out",
+        },
+        "-=0.2"
+      );
+  });
+
   return (
     <section className="bg-main-bg">
       <div className="hero-container">
         <img
-          src="/images/hero-img.png"
+          src="/images/static-img.png"
           alt="hero-bg"
           className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150"
         />
-        <div className="hero-content">
+        <div className="hero-content opacity-0">
           <div className="overflow-hidden">
             <h1 className="hero-title">Freaking delicious</h1>
           </div>
@@ -22,7 +58,14 @@ const Hero = () => {
             </div>
           </div>
 
-          <h2></h2>
+          <h2>
+            Live life to the fullest with SPYLT: Shatter boredom and embrace
+            your inner kid with every deliciously smooth chug.
+          </h2>
+
+          <div className="hero-button">
+            <p>Chug the SPYLT</p>
+          </div>
         </div>
       </div>
     </section>
