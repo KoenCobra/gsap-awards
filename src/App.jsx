df492471021f56194e0e3fbcf0471373ad98fb49
Koen De Groot <koendegroot84@gmail.com>
2025-11-01 14:46:35 +0100

    Update dependencies and configure Tailwind CSS integration
    - Added @gsap/react, gsap, @tailwindcss/vite, react-responsive, and tailwindcss to dependencies in package.json.
    - Updated vite.config.js to include Tailwind CSS plugin.
    - Modified App.jsx to include NavBar and Hero components for improved layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <NavBar />
+      <Hero />
+    </main>
+  );
 };
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+const Hero = () => {
+  return (
+    <section className="bg-main-bg">
+      <div className="hero-container">
+        <img
+          src="/images/hero-img.png"
+          alt="hero-bg"
+          className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150"
+        />
+        <div className="hero-content">
+          <div className="overflow-hidden">
+            <h1 className="hero-title">Freaking delicious</h1>
+          </div>
+          <div
+            style={{
+              clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+            }}
+            className="hero-text-scroll"
+          >
+            <div className="hero-subtitle">
+              <h1>Protein + Caffeine</h1>
+            </div>
+          </div>
+
+          <h2></h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
